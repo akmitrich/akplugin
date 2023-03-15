2066e36fe948f47014ef0607057ed30f70234109
akmitrich <ak.mitrich@mail.ru>
2023-03-15 12:05:14 +0300

    Audio stream read stub

--- src/audio_stream.rs
+++ src/audio_stream.rs
@@ -2,12 +2,12 @@ use crate::uni;
 
 pub static STREAM_VTABLE: uni::mpf_audio_stream_vtable_t = uni::mpf_audio_stream_vtable_t {
     destroy: Some(stream_destroy),
-    open_rx: None,
-    close_rx: None,
-    read_frame: None,
-    open_tx: Some(stream_open),
-    close_tx: Some(stream_close),
-    write_frame: Some(stream_write),
+    open_rx: Some(stream_open),
+    close_rx: Some(stream_close),
+    read_frame: Some(stream_read),
+    open_tx: None,
+    close_tx: None,
+    write_frame: None,
     trace: Some(trace),
 };
 
@@ -26,9 +26,9 @@ pub unsafe extern "C" fn stream_close(_stream: *mut uni::mpf_audio_stream_t) -> 
     uni::TRUE
 }
 
-pub unsafe extern "C" fn stream_write(
+pub unsafe extern "C" fn stream_read(
     _stream: *mut uni::mpf_audio_stream_t,
-    _frame: *const uni::mpf_frame_t,
+    _frame: *mut uni::mpf_frame_t,
 ) -> uni::apt_bool_t {
     uni::TRUE
 }
